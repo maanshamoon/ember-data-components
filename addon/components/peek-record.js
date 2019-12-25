c5fb61c41c6ac0a09abf11ae0e597ef27b00af1e
Maan Shamoon <maanshamoon@yahoo.com>
2019-12-24 22:42:29 -0800

    Cleaned up peekRecord component logic

--- addon/components/peek-record.js
+++ addon/components/peek-record.js
@@ -1,19 +1,10 @@
 import Component from '@glimmer/component';
 import { inject as service } from "@ember/service";
-import { tracked } from '@glimmer/tracking';
 
 export default class PeekRecordComponent extends Component {
 	@service store;
-	@tracked isLoading = true;
-	@tracked hasError = false;
 
 	get data() {
-		return this.store.peekRecord(this.args.modelName, this.args.id)
-			.then(() => {
-				this.isLoading = false;
-			})
-			.catch(() => {
-				this.hasError = true;
-			});
+		return this.store.peekRecord(this.args.modelName, this.args.id);
 	}
 }
