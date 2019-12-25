93d0035ff59be80b2883eb1b344377e6410906b7
Maan Shamoon <maanshamoon@yahoo.com>
2019-12-24 23:18:44 -0800

    Added isLoading and hasError to the query component api

--- addon/components/query.js
+++ addon/components/query.js
@@ -1,12 +1,15 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { tracked } from '@glimmer/tracking';
 
 export default class QueryComponent extends Component {
 	@service store;
+	@tracked isLoading = true;
+	@tracked hasError = false;
 
 	get data() {
 		return this.store.query(this.args.modelName, this.args.params)
-			.then(() => this.isLoading = false)
-			.catch(() => this.hasError = true);
+		.then(() => this.isLoading = false)
+		.catch(() => this.hasError = true);
 	}
 }
